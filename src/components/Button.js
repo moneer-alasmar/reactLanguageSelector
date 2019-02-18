922e4909684fe77c760cca6d2f4b2b51345b5c36
Moneer <kiraalasmar@gmail.com>
2019-02-18 13:21:49 -0800

    implemented consumer

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,10 +2,18 @@ import React, { Component } from "react";
 import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends Component {
-  static contextType = LanguageContext;
+  renderSubmit(value) {
+    return value === "english" ? "Submit" : "Voorleggen";
+  }
+
   render() {
-    console.log(this.context);
-    return <button className="ui button primary">Submit</button>;
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {value => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,3 +1,3 @@
 import React from "react";
 
-export default React.createContext("arabic");
+export default React.createContext("english");
